97d83e77437724a5a6e00fac7e32393af86ab1fd
Barensel Baş <58819838+barensel@users.noreply.github.com>
2022-05-06 00:37:46 +0300

    Update snail-app.py

--- snail-app.py
+++ snail-app.py
@@ -617,7 +617,7 @@ if sequence in df["Gene"].unique():
             link = f'[{text_string_variable2}]({url_string_variable2} ) '
             st.sidebar.markdown(link, unsafe_allow_html=True)
             
-            text_string_variable3= "--------Medium--------"
+            text_string_variable3= "-------Medium-------"
             url_string_variable3 = "https://medium.com/@haner11/snail-date-app-via-snail-trail-from-chambers-690a2aee89bc"
             link = f'[{text_string_variable3}]({url_string_variable3} ) '
             st.sidebar.markdown(link, unsafe_allow_html=True)
